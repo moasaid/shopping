65448c33ba96c177b783858366e02bae1042318f
Mohamed Said <moasaid9@gmail.com>
2020-03-03 11:37:39 +0000

    Initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="shopping in shopping_list" :key="shopping.id">
+      <div class="card-content">
+        <i class="material-icons delete" @click="deleteShopping(shopping.id)">delete</i>
+        <h2>{{ shopping.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(item, index) in shopping.items" :key="index" >
+            <span class="chip">{{ item }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      shopping_list: [
+        {title: 'weekly shop', slug: 'weekly-shop', items:['bread', 'oat meal', 'jam', 'eggs'], id:'1'},
+        {title: 'picnic', slug: 'picnic', items:['coffee', 'sugar', 'scons', 'rice'], id:'2'},
+        {title: 'car trip', slug: 'car-trip', items:['cake', 'water', 'snikers', 'magazine'], id:'3'}
+      ]
+    }
+  },
+  methods: {
+    deleteShopping(id){
+      this.shopping_list = this.shopping_list.filter(shopping => {
+        return shopping.id != id
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .index{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 30px;
+    margin-top: 60px;
+  }
+
+  .index h2{
+    font-size: 1.8em;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 0;
+    color: #1565C0;
+  }
+
+  .index .ingredients{
+    margin: 30px auto;
+  }
+
+  .index .ingredients li{
+    display: inline-block;
+ }
+  .index .delete {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    cursor: pointer;
+    color: #aaa;
+    font-size: 1.4em;
+  }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class='navbar'>
+    <nav class='nav-extended blue darken-3'>
+      <div class='nav-content'>
+        <router-link to="">
+          <span class='nav-title'>Your Shopping List</span>
+        </router-link>
+        <a href="" class='btn-floating btn-large halfway-fab amber accent-4'>
+          <router-link to="">
+            <i class='material-icons'>add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    data(){
+      return{}
+    },
+  }
+</script>
+
+<style>
+  .navbar nav{
+    padding: 0 20px;
+  }
+
+  .nav-extended .nav-content{
+    text-align: left;
+    padding: 0 60px;
+  }
+
+  .nav-title{
+    font-weight: bold;
+  }
+</style>
