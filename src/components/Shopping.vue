2ea46e58e89c7130a439899c07fd1c24c264dfff
Mohamed Said <moasaid9@gmail.com>
2020-03-05 07:54:54 +0000

    Fixed add form css

--- src/components/Shopping.vue
+++ src/components/Shopping.vue
@@ -2,12 +2,12 @@
   <div class="shopping container">
     <h2>Add Shopping List</h2>
     <form>
-      <div class="field title row">
-        <label for="title">Shopping List Name:</label>
+      <div class="field title">
+        <label class="left" for="title">Shopping List Name:</label>
         <input type="text" name="title" v-model="title">
       </div>
-      <div class="field item row">
-        <label for="item">Add Item:</label>
+      <div class="field item">
+        <label class="left" for="item">Add Item:</label>
         <input type="text" name="item" v-model="item">
       </div>
       <div>
@@ -21,11 +21,29 @@
   export default {
     name: 'Shopping',
     data(){
-      return{}
+      return{
+        title: null
+      }
     },
   }
 </script>
 
 <style>
+  .shopping{
+    margin-top: 60px;
+    padding: 20px;
+    max-width: 500px;
+  }
+
+  .shopping h2 {
+    font-size: 2em;
+    margin: 20px auto;
+    padding-bottom: 20px;
+    font-weight: bold;
+  }
+
+  .shopping .field {
+    margin: 20px auto;
+  }
 
 </style>
