8a20a8d762e24303bec6f2228072c882ce562aa7
Mohamed Said <moasaid9@gmail.com>
2020-03-15 12:35:55 +0000

    Revert "added edit shopping"
    This reverts commit e6c8cf74a5975f6820b0e4b62413af116e594951.

--- src/components/EditShopping.vue
+++ src/components/EditShopping.vue
@@ -1,33 +1,12 @@
 <template>
-  <div>
-    <div v-if="shopping">
-
-    </div>
-    <div class="edit_shopping">
-      <h3>Edit: {{ shopping.title }}</h3>
-    </div>
+  <div class="edit_shopping">
+    <h3>Edit:  {{ this.$route.params.shopping_slug }}</h3>
   </div>
 </template>
 
 <script>
-  import db from '@/firebase/init'
-
   export default {
-  name: 'EditShopping',
-    data(){
-    return{
-      shopping: null
-    }
-    },
-    created() {
-    let ref = db.collection('shopping_list').where('slug', '==', this.$route.params.shopping_slug)
-      ref.get().then(snapshot => {
-        snapshot.forEach(doc => {
-          this.shopping = doc.data()
-          this.shopping.id = doc.id
-        })
-      })
-    }
+  name: 'EditShopping'
   }
 </script>
 
